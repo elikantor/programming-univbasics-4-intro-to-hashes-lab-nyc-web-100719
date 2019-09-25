9905b099d6b239fcad939ab9dd09aa8691338835
Eli Kantor <eli.kantor@outlook.com>
2019-09-25 21:50:07 +0000

    Done.

--- intro_to_ruby_hashes_lab.rb
+++ intro_to_ruby_hashes_lab.rb
@@ -24,18 +24,19 @@ end
 
 def my_hash_creator(key, value)
   hash1 = {
-    #{key}: value
+    key => value
   }
 end
 
 def read_from_hash(hash, key)
-  return hash[:key]
+  return hash[key]
 end
 
 def update_counting_hash(hash, key)
-   if hash[:key] == nil 
-     hash[:key] = 1 
+   if hash[key] == nil 
+     hash[key] = 1 
     else 
-    hash[:key] += 1 
+    hash[key] += 1 
     end
+    return hash
 end
